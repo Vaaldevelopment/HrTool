51e256e5ee4d274970a87c8330cd4a62aa598906
Pravin <pravin.chopde@vaal-triangle.com>
2019-01-21 17:58:00 +0530

    Services

---
+++ src/app/modules/new-requirement-module.ts
@@ -0,0 +1,19 @@
+export class NewRequirement {
+    reqId: any;
+    jobTitle: any;
+    numOfPos: any;
+    department: any;
+    jobType: any;
+    budget: any;
+    experience: any;
+    openingDate: Date;
+    closingDate: Date;
+    location: any;
+    jdFile: any;
+    requestTo: any;
+    aptiFile: any;
+    machineTestFile: any;
+    tests: any;
+    notes: any;
+    presets: any;
+}
